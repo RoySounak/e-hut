e28e63fb2d28bf7a4f2611b02ef532bf19b99e93
RoySounak <roy.sounak01@gmail.com>
2020-10-08 12:10:04 +0530

    'initial'

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+
+
+app.get('/home', (req,res)=>{
+    res.render('home')
+})
+
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/js/views'))
+app.set('view engine','ejs')
+
+app.listen(PORT, () =>{
+    console.log(`Listining on ${PORT}`)
+})
